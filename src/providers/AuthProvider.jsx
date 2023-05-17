3c089c1fbbe33df64e5ddcd1e12d92c368881fab
AkhiSharmin <akhisharmin100@gmail.com>
2023-05-17 13:22:27 +0600

    all done

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -48,7 +48,7 @@ const AuthProvider = ({ children }) => {
         const loggedUser = {
           email: currentUser.email,
         };
-        fetch("http://localhost:5000/jwt", {
+        fetch("https://car-doctor-server-akhisharmin.vercel.app/jwt", {
           method: "POST",
           headers: {
             "content-type": "application/json",
